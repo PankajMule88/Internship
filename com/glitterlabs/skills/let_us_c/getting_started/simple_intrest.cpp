d8e171d3d77784ab6b29437b0aff05553f014fad
PankajMule88 <pankajmuley10688@gmail.com>
2016-02-27 18:44:12 +0530

    My first commit with new repository.
    I have deleted my  old repository because of some issue,
    here I have commit (Getting started, The decision control structure ,The
    loop control structure and Fuctions and pointers)

---
+++ com/glitterlabs/skills/let_us_c/getting_started/simple_intrest.cpp
@@ -0,0 +1,14 @@
+/*
+ * simple_intrest.cpp
+ *
+ *  Created on: Feb 26, 2016
+ *      Author: user
+ */
+//program for simlpe interest
+
+void main()
+{
+int p=10,n=13;
+float r=9.2, si=p*n*r/100;
+printf("interest = %f",si);
+}
